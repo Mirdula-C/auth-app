ad6cc83cd958738fdbce4b03bdfe8b18f290bf3a
Mirdula-C <mirdula311204@gmail.com>
2025-03-25 15:13:01 +0530

    Fix MongoDB warnings and add root route

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Auth API!");
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
